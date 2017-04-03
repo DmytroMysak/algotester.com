ff2555efc01a44031c0d498062d5cca846b0ee10
DmytroMysak <dmytro.mysak@gmail.com>
2017-04-04 00:39:13 +0300

    add 2 answers

---
+++ PokemonName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PokemonName
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var t = Console.ReadLine();
+            var s = Console.ReadLine();
+            var res = false;
+            for (var i = 0; i < t.Length; i++)
+            {
+                if (t[i] != s[0] && t[i] != '?') continue;
+                var ss = true;
+                for (int j = 1, k = i + 1; j < s.Length; ++j, ++k)
+                {
+                    if (k != t.Length && (t[k] == s[j] || t[k] == '?')) continue;
+                    ss = false;
+                    break;
+                }
+                if (!ss) continue;
+                res = true;
+                break;
+            }
+            Console.WriteLine(res ? "YES" : "NO");
+        }
+    }
+}
---
+++ Teeth/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Teeth
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var k = long.Parse(Console.ReadLine().Split(' ')[1]);
+            var a = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt64);
+            long max = 0;
+            long t = 0;
+            foreach (var l in a)
+            {
+                if (l >= k)
+                {
+                    ++t;
+                }
+                else
+                {
+                    t = 0;
+                }
+                if (t > max)
+                {
+                    max = t;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
